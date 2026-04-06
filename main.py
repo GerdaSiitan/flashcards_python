ec81f32a2a5965db2c29822a5313f2614bce7b57
gerdasiitan <gerda.siitan13@gmail.com>
2026-04-06 22:13:16 +0300

    just a test of ttkbootstrap and tkinter

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-import tkinter
+import tkinter as tk
+import ttkbootstrap as ttk
+
+class App(ttk.Window):
+    def __init__(self):
+        super().__init__()
+        self.title("Flashcards App")
+        self.geometry("1080x720")
+
+
+        self.button = ttk.Button(self, text="Click Me", command=self.on_button_click)
+        self.button.pack(pady=20)
+
+    def on_button_click(self):
+        print("Button clicked!")
+
+root = App()
+root.mainloop()
